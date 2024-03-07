cc66b55dfdf2bcfc281ab543121ecd076317d0c7
satoteharshal97 <satoteharshal97@gmail.com>
2024-03-07 23:18:10 +0530

    Property Dealers

--- src/main/java/com/mycompany/propertymanagement/PropertyManagementApplication.java
+++ src/main/java/com/mycompany/propertymanagement/PropertyManagementApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PropertyManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PropertyManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(PropertyManagementApplication.class, args
+        );
+    }
 
 }
